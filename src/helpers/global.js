06e91f26321ae4ffc1419b5a80708db2a34d78b3
guptadk <dkgupta8953@gmail.com>
2023-01-02 15:01:01 +0530

    newcode

--- src/helpers/global.js
+++ src/helpers/global.js
@@ -1,4 +1,4 @@
-export const URL = 'http://localhost:3000/api/';
-// export const URL ='https://api.tradekia.com/api/';                       
+// export const URL = 'http://localhost:3000/api/';
+export const URL ='https://api.tradekia.com/api/';                       
 // export const URL ='https://testapi.tradekia.com/api/'; 
 // export const URL ='https://chat.tradekia.com/api/'; 
